d5cd4e639b94b1aecdc3f82662ea62bc6e451b93
AlexReiner <56170690+bio-Arc@users.noreply.github.com>
2023-03-03 12:18:06 -0500

    Basic q learning structure written minus performing action and receiving reward

---
+++ HygroDesign.Grasshopper/DefineDQLTrainingBehaviour.cs
@@ -0,0 +1,47 @@
+using Grasshopper;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using Rhino;
+using System;
+using System.Collections.Generic;
+
+using HygroDesign.Core;
+using ABxM.Core;
+using ABxM.Core.Agent;
+using ABxM.Core.AgentSystem;
+using ABxM.Core.Behavior;
+
+namespace HygroDesign.Grasshopper.Components
+{
+
+    public class DQLTraining : GH_Component
+    {
+        DQLTraining dqlTrainingBehaviour = new DQLTraining();
+
+        public DQLTraining()
+          : base("DQLTrainingBehaviour", "DQLTraining",
+            "Define a behaviour for Deep Q Learning for CrossSectionAgent",
+            "HygroDesign", "Design")
+        {
+        }
+
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+        }
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Behaviour", "B", "The Deep Q Learning Training Behaviour", GH_ParamAccess.item);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            dqlTrainingBehaviour.Update();
+            DA.SetData("Behaviour", dqlTrainingBehaviour);
+        }
+        public override GH_Exposure Exposure => GH_Exposure.primary;
+        protected override System.Drawing.Bitmap Icon => null;
+        public override Guid ComponentGuid => new Guid("BDCC42BC-EE8A-4343-BDF0-320F082A0A42");
+    }
+}
